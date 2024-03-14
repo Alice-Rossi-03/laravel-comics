3f26a08d1ae2afff968bcbf32b50b5541797bcc5
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-03-14 16:40:34 +0100

    fix(header) /jumbotron

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,8 +5,8 @@
 
 @section('content')
 
-<main style="background-color: white;">
-    <h2>New page</h2>
+<main style="background-color: #1C1C1C; color: white; padding-block: 2em;">
+    <h2 style="text-align: center; margin: 0;">New page</h2>
 </main>
 
 @endsection
